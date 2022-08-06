2a093abe9c8986191b5533ebde0c670de0d60059
Gesbien <72868525+Gesbien@users.noreply.github.com>
2022-08-06 12:05:23 -0400

    Prolog
    Terminacion de las caracteristicas de las aves

--- proyecto_final_grupo1.pl
+++ proyecto_final_grupo1.pl
@@ -3,7 +3,7 @@
 
 % elegir cantidad de intentos, si se acaban, se termina la ronda
 
-adivina(P,buho):- 
+adivina(P,buho):-
     tamano(P,pequeno),
     tamano_pico(P,pequeno),
     color_plumaje(P,marron),
@@ -20,38 +20,75 @@ adivina(P,ganso):-
     alimentacion(P,omnivoros),
     puede_nadar(P),
     puede_volar(P),
+    puede_caminar(P),
     migran(P).
-adivina(P,gallina):-
-    tamano(P,mediano),
-    tamano_pico(P,mediano),
+adivina(P,gallo):-
+    tamano(P,pequeno),
+    tamano_pico(P,pequeno),
     color_plumaje(P,blanco),
     color_pico(P,amarillo),
     alimentacion(P,omnivoros),
-    puede_correr(P).
+    puede_caminar(P).
 adivina(P,aguila):-
-    tamano(P,grande),
-    tamano_pico(P,mediano),
-    color_plumaje(P,blanco),
+    tamano(P,mediano),
+    tamano_pico(P,grande),
+    color_plumaje(P,marron),
     color_pico(P,amarillo),
-    carnivoro(P),
+    alimentacion(P,carnivoro),
     puede_volar(P),
     migran(P).
 adivina(P,flamenco):-
-    tamano(P,mediano),
-    tamano_pico(P,mediano),
-    color_plumaje(P,blanco),
-    color_pico(P,amarillo),
-    carnivoro(P).
+    tamano(P,grande),
+    tamano_pico(P,grande),
+    color_plumaje(P,rosado),
+    color_pico(P,blanco_negro),
+    alimentacion(P,omnivoro),
+    puede_volar(P),
+    puede_caminar(P),
+    migran(P).
 adivina(P,loro):-
+    tamano(P,pequeno),
+    tamano_pico(P,mediano),
+    color_plumaje(P,rojo),
+    color_pico(P,blanco),
+    alimentacion(P,omnivoro),
+    puede_volar(P),
+    puede_caminar(P),
+    migran(P).
+adivina(P,tucan):-
+    tamano(P,pequeno),
+    tamano_pico(P,grande),
+    color_plumaje(P,negro),
+    color_pico(P,naranja),
+    alimentacion(P,herbivoros),
+    puede_volar(P),
+    migran(P).
+adivina(P,cisne_negro):-
     tamano(P,mediano),
     tamano_pico(P,mediano),
-    color_plumaje(P,blanco),
-    color_pico(P,amarillo).
-
-% adivina(P,tucan).
-% adivina(P,cisne_negro).
-% adivina(P,pinguino).
-% adivina(P,avestruz).
+    color_plumaje(P,negro),
+    color_pico(P,rojo),
+    alimentacion(P,herbivoro),
+    puede_nadar(P),
+    puede_volar(P),
+    puede_caminar(P),
+    migran(P).
+adivina(P,pinguino):-
+    tamano(P,mediano),
+    tamano_pico(P,mediano),
+    color_plumaje(P,blanco_negro),
+    color_pico(P,amarillo),
+    alimentacion(P,carnivoro),
+    puede_nadar(P),
+    puede_caminar(P),
+    migran(P).
+adivina(P,avestruz):-
+    tamano(P,grande),
+    tamano_pico(P,mediano),
+    color_plumaje(P,negro),
+    color_pico(P,amarillo),
+    alimentacion(P,herbivoro),
+    puede_caminar(P).
 
 visualiza_pregunta(A):-write(A), write('(si/no)? ').
 pregunta(A,Resp):-visualiza_pregunta(A), read(Resp).
@@ -59,16 +96,16 @@ pregunta(A,Resp):-visualiza_pregunta(A), read(Resp).
 responde(si, A):-!,assertz(cierto(A)).
 responde(no, A):-!,assertz(falso(A)), fail.
 
+preguntable(nocturno(_)).
+preguntable(puede_nadar(_)).
+preguntable(puede_volar(_)).
+preguntable(puede_caminar(_)).
+preguntable(migran(_)).
 preguntable(tamano(_,_)).
 preguntable(tamano_pico(_,_)).
 preguntable(color_plumaje(_,_)).
 preguntable(color_pico(_,_)).
-preguntable(nocturno(_)).
 preguntable(alimentacion(_,_)).
-preguntable(puede_nadar(_)).
-preguntable(puede_volar(_)).
-preguntable(puede_correr(_)).
-preguntable(migran(_)).
 
 resuelve(true):-!.
 resuelve((A,B)):-!,resuelve(A), resuelve(B).
@@ -76,5 +113,3 @@ resuelve(A):-falso(A), !, fail.
 resuelve(A):-cierto(A),!.
 resuelve(A):-clause(A,B),resuelve(B).
 resuelve(A):-preguntable(A),pregunta(A,Resp),responde(Resp,A).
-
-imagen(buho,'/img/buho.jpg').
